5f86daec5f06902203caf96b93b745eda21e11d5
WangJie <i@i8e.net>
2018-10-14 12:29:49 +0800

    add type definitions

---
+++ index.d.ts
@@ -0,0 +1,32 @@
+/* =================== USAGE ===================
+
+    import * as conf from "@cnwangjie/conf";
+    conf().load("config.json");
+
+ =============================================== */
+
+declare function Conf(): Conf.Config
+
+declare namespace Conf {
+  interface ArgsOption {
+    transform?: boolean
+    args?: Array<string>
+  }
+  interface EnvOption {
+    transform?: boolean
+    split?: boolean
+    tolower?: boolean
+  }
+  interface Config {
+    (path: string): any
+    get(path: string): any
+    set(path: string, value: any): void
+    load(path: string): this
+    load(path: string, resolver: Function): this
+    args(opt: ArgsOption): this
+    env(opt: EnvOption): this
+    separator: string // default '.'
+  }
+}
+
+export = Conf
